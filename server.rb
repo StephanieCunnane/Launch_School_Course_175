ed12d153ceb0ca17897a0e545169861380d12b13
Stephanie Cunnane <stephanie.cunnane@gmail.com>
2019-11-07 19:15:24 -0800

    Remove debugging output

--- server.rb
+++ server.rb
@@ -18,24 +18,33 @@ loop do
 
   request_line = client.gets
   next if request_line =~ /favicon/
-  puts request_line
 
   http_method, path, params = parse_request(request_line)
 
-  puts http_method
-  puts path
-  puts params
 
   client.puts "HTTP/1.1 200 OK"
-  client.puts "Content-Type: text/plain\r\n\r\n"
-  client.puts request_line
+  client.puts "Content-Type: text/html\r\n\r\n"
+
+  client.puts "<html>"
+  client.puts "<body>"
+  client.puts "<pre>"
+  client.puts http_method
+  client.puts path
+  client.puts params
+  client.puts "</pre>"
+
+  client.puts "<h1>Rolls!</h1>"
 
   rolls = params['rolls'].to_i
   sides = params['sides'].to_i
 
   rolls.times do
-    client.puts rand(sides) + 1
+    roll = rand(sides) + 1
+    client.puts "<p>", roll, "</p>"
   end
+
+  client.puts "</body>"
+  client.puts "</html>"
 
   client.close
 end
